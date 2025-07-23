9afaf59015ececf43975ba3e86fc877ff5cd1806
fleddd <fedkiv20172@gmail.com>
2025-07-23 14:51:52 +0300

    [feat] added older projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,27 @@
 export const projects = [
 	{
+		name: 'Real time Chat Messenger',
+		description: 'Full stack RealTime Chat Messenger app with user authentication. Not finished yet.',
+		stack: [
+			'HTML',
+			'CSS',
+			'Typescript',
+			'React',
+			'TailwindCSS',
+			'Node.js',
+			'Express',
+			'MongoDB',
+			'Mongoose',
+			'Socket.IO',
+			'JWT',
+		],
+		image: '',
+		link: {
+			public: '',
+			github: 'https://github.com/fleddd/realtimemessanger',
+		},
+	},
+	{
 		name: 'Notes',
 		description: 'Full stack CRUD Notes app connected to MS SQL database.',
 		stack: [
@@ -16,6 +38,24 @@ export const projects = [
 		image: '',
 		link: {
 			public: 'https://fleddd.github.io/notes',
+			github: 'https://github.com/fleddd/notes',
+		},
+	},
+	
+	{
+		name: 'Employee Management System',
+		description: 'Full stack CRUD Employee Management System app connected to Firebase',
+		stack: [
+			'HTML',
+			'CSS',
+			'Javascript',
+			'React',
+			'TailwindCSS',
+			'Firestore',
+		],
+		image: '',
+		link: {
+			public: "https://fleddd.github.io/UserProfile/",
 			github: 'https://github.com/fleddd/notes',
 		},
 	},
